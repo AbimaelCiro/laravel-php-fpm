9ac7a824b75f592e1939a83651274d2310ee6408
Abimael Ciro MAMANI GARCIA <44014578+AbimaelCiro@users.noreply.github.com>
2024-09-19 13:01:29 -0500

    sd

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ fi
 if [ $tag != 'latest' ]
 then
   echo 'Build from from tag'
-  docker build -f src/docker/${tag}/Dockerfile -t jkaninda/laravel-php-fpm:$tag .
+  docker build -f src/docker/${tag}/Dockerfile -t AbimaelCiro/laravel-php-fpm:$tag .
 else
  echo 'Build latest'
- docker build -f src/docker/8.3/Dockerfile -t jkaninda/laravel-php-fpm:$tag .
+ docker build -f src/docker/8.3/Dockerfile -t AbimaelCiro/laravel-php-fpm:$tag .
  
 fi
 
